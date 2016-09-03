d72809e8655addbb6acaa704cdaa0a65a798a4c6
iconoctopus <basileus.autocrator@gmail.com>
2016-09-03 13:32:18 +0200

    Extension des tests automatiques

---
+++ test/org/duckdns/spacedock/commonutils/PropertiesHandlerTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.duckdns.spacedock.commonutils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author iconoctopus
+ */
+public class PropertiesHandlerTest
+{
+
+    @Test
+    public void testgetString()
+    {
+	Assert.assertEquals("erreur d'accès à un fichier de propriétés", PropertiesHandler.getInstance("meugni").getString("pout pouti"));
+	Assert.assertEquals("indice", PropertiesHandler.getInstance("commonutils").getString("indice"));
+    }
+
+    @Test
+    public void testgetErrorMessage()
+    {
+	Assert.assertEquals("erreur d'accès à un fichier de propriétés", PropertiesHandler.getInstance("meugni").getErrorMessage("pout pouti"));
+	Assert.assertEquals("paramétre aberrant:", PropertiesHandler.getInstance("commonutils").getErrorMessage("param_aberr"));
+    }
+}
